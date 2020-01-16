66600925dbcdf3f4f9704bc3be35bf317ee4b60c
Suvorov Aleksey <suvorov@hostco.ru>
2020-01-16 18:24:04 +0300

    Updates

---
+++ precache-manifest.b900a0b0d7213d868b43608bcf1383df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b24c96f6b44d27ca4c9189fde12793",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "675d9e68cef17be2f24c",
+    "url": "/my-react-app/static/css/main.8e915891.chunk.css"
+  },
+  {
+    "revision": "9104592366618e5b2deb",
+    "url": "/my-react-app/static/js/2.b85f5e17.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-react-app/static/js/2.b85f5e17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "675d9e68cef17be2f24c",
+    "url": "/my-react-app/static/js/main.11628d01.chunk.js"
+  },
+  {
+    "revision": "041cdc0f101ec32576dc",
+    "url": "/my-react-app/static/js/runtime-main.508c03f7.js"
+  }
+]);
